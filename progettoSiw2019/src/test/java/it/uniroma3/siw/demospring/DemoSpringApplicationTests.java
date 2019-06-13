ff99bf4cab8d4433e59833c3dfac2db5095bb35a
Sofia <sofy.perty96@gmail.com>
2019-06-13 14:33:30 +0200

    primo commit

---
+++ progettoSiw2019/src/test/java/it/uniroma3/siw/demospring/DemoSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package it.uniroma3.siw.demospring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
