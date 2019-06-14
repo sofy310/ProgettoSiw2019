780cdabdbd21e38d4bd16aa145c18885ce9c6d2e
Sofia <sofy.perty96@gmail.com>
2019-06-14 10:12:31 +0200

    Classi validator aggiunte

--- progettoSiw2019/src/main/java/it/uniroma3/siw/demospring/validator/FotografiaValidator.java
+++ progettoSiw2019/src/main/java/it/uniroma3/siw/demospring/validator/FotografiaValidator.java
@@ -1,5 +1,25 @@
 package it.uniroma3.siw.demospring.validator;
 
-public class FotografiaValidator {
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
 
+import it.uniroma3.siw.demospring.model.Fotografia;
+
+
+@Component 
+public class FotografiaValidator implements Validator{
+
+	@Override
+	public boolean supports(Class<?> aClass) {
+		return Fotografia.class.equals(aClass);
+	}
+
+	@Override
+	public void validate(Object o, Errors error) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(error, "nome", "required");		
+
+	}
+	
 }
