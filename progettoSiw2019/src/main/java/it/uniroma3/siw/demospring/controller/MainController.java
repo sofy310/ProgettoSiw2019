2df447b2fe873f6e29d629db1d8d75610a23b506
Sofia <sofy.perty96@gmail.com>
2019-06-15 14:11:08 +0200

    modificata galleriaFoto.html, aggiunto for each

--- progettoSiw2019/src/main/java/it/uniroma3/siw/demospring/controller/MainController.java
+++ progettoSiw2019/src/main/java/it/uniroma3/siw/demospring/controller/MainController.java
@@ -1,11 +1,18 @@
 package it.uniroma3.siw.demospring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.uniroma3.siw.demospring.services.FotografiaService;
+
 @Controller
 public class MainController {
+	
+	@Autowired
+	private FotografiaService fotografiaService;
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String getIndex() {
@@ -13,7 +20,8 @@ public class MainController {
 	}
 	
 	@RequestMapping(value="/getGalleria")
-	public String getGalleria() {
+	public String getGalleria(Model model) {
+		model.addAttribute("fotografie", this.fotografiaService.tutte());
 		return "galleriaFoto";
 	}
 
