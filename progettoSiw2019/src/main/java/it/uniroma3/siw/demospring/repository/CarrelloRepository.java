277dcc102744d78bbb25ff12bcff5f71ae07c4e1
Tiziano Taroni <tiziano.taroni@gmail.com>
2019-06-17 07:12:16 +0200

    DeleteDalCarrello

--- progettoSiw2019/src/main/java/it/uniroma3/siw/demospring/repository/CarrelloRepository.java
+++ progettoSiw2019/src/main/java/it/uniroma3/siw/demospring/repository/CarrelloRepository.java
@@ -1,9 +1,13 @@
 package it.uniroma3.siw.demospring.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import it.uniroma3.siw.demospring.model.Fotografia;
 import it.uniroma3.siw.demospring.model.FotografiaCarrello;
 
 public interface CarrelloRepository extends CrudRepository<FotografiaCarrello, Long>{
 
+	public List <FotografiaCarrello> delete (Fotografia Fotografia);
 }
--- progettoSiw2019/src/main/java/it/uniroma3/siw/demospring/services/CarrelloService.java
+++ progettoSiw2019/src/main/java/it/uniroma3/siw/demospring/services/CarrelloService.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.uniroma3.siw.demospring.model.Fotografia;
 import it.uniroma3.siw.demospring.model.FotografiaCarrello;
 import it.uniroma3.siw.demospring.repository.CarrelloRepository;
 
@@ -24,5 +25,10 @@ public class CarrelloService {
 	@Transactional
 	public List<FotografiaCarrello> tutte() {
 		return(List<FotografiaCarrello>)this.carrelloRepository.findAll();
+	}
+	
+	@Transactional
+	public List <FotografiaCarrello> delete (Fotografia fotografia){
+		return this.carrelloRepository.delete(fotografia);
 	}
 }
