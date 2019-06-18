504bed1d8aa7127fb472c6000a017ae81612017e
Tiziano Taroni <tiziano.taroni@gmail.com>
2019-06-18 18:40:56 +0200

    Update 2

---
+++ progettoSiw2019/src/main/java/it/uniroma3/siw/demospring/controller/FunzionarioController.java
@@ -0,0 +1,11 @@
+package it.uniroma3.siw.demospring.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.uniroma3.siw.demospring.model.Fotografo;
+
+public class FunzionarioController {
+	
+
+}
